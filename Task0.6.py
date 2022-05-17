88856c8946777e27c6da0bd9edf628d4091b9f4b
mantoi4 <91872963+mantoi4@users.noreply.github.com>
2022-05-17 09:55:42 +0200

    Delete Task0.6.py

--- Task0.6.py
+++
@@ -1,9 +0,0 @@
-def maximum_number():
-    numbers = [1,2,3]
-    return max(numbers)
-
-a = int(input("Enter First number"))
-b = int(input("Enter Second number"))
-c = int(input("Enter Third number"))
-
-print("maximum_number ::>",max(a, b, c))
