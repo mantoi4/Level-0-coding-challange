78c288d94883dd7d7a1d2e7ae93982e2a24ec647
mantoi4 <91872963+mantoi4@users.noreply.github.com>
2022-05-17 09:57:28 +0200

    Delete Task0.7.1py.py

--- Task0.7.1py.py
+++
@@ -1,9 +0,0 @@
-def Fahrenheit_to_Celsius():
-
- Fahrenheit = int(input("Enter temperature in Fahrenheit: "))
-
- Celsius = (Fahrenheit - 32) * 5.0 / 9.0
-
- print("Temperature:", Fahrenheit, "Fahrenheit = ", Celsius, " C")
-
-Fahrenheit_to_Celsius()
