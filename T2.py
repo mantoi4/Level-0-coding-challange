1f5534f19dc1bcadfb6e827f65336db286c3cbfa
mantoi4 <91872963+mantoi4@users.noreply.github.com>
2022-05-19 07:04:55 +0200

    Add files via upload

---
+++ T2.py
@@ -0,0 +1,11 @@
+x = 1 + 1 * 2
+y = (1 + 1) * 2
+z = 1 + ( 1 * 2 )
+a = 1 + 1 * 2 / 2
+b = (1 + 1 * 2 ) /  2
+
+print(f'x = {x}')
+print(f'y = {y}')
+print(f'z = {z}')
+print(f'a = {a}')
+print(f'b = {b}')
